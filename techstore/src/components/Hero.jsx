3dfa125464dae79798d7cbfc2b65c4b722741b81
giuseppebatti <battigiuseppe6@gmail.com>
2025-11-19 11:43:23 +0100

    modifiche Hero

--- techstore/src/components/Hero.jsx
+++ techstore/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Hero({ title, subtitle, ctaText, ctaLink }) {
   return (
     <section
@@ -9,13 +11,14 @@ export default function Hero({ title, subtitle, ctaText, ctaLink }) {
     >
       <h1 className="text-3xl md:text-5xl font-bold mb-4 animate-fade-down">{title}</h1>
       <p className="text-lg md:text-2xl mb-8 animate-fade-up">{subtitle}</p>
+
       {ctaText && (
-        <a
-          href={ctaLink}
+        <Link
+          to={ctaLink}
           className="inline-block bg-yellow-300 text-black px-6 py-3 rounded-lg font-semibold hover:bg-yellow-200 hover:scale-105 transition shadow-lg"
         >
           {ctaText}
-        </a>
+        </Link>
       )}
     </section>
   );
